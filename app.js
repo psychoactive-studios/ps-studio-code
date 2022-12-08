a67151c362ddacebdb5ca744e76c8301a1ede7a2
axbwh <bannwarth.alexandre@gmail.com>
2022-12-08 15:01:09 +1300

    no careers

--- app.js
+++ app.js
@@ -3,20 +3,20 @@ import initCms from "./js/global/initCms"
 import logCareers from "./js/global/logCareers"
 import { loopLogoLoading, readyPreloader } from "./js/global/preloader"
 import initProjectLotties from "./js/global/projectLotties"
+import loadAnim from "./js/home/loadAnim"
 
-const parceled = true
+const parceled = true // for checking localhost vs github pages / CDN
 
-// global dictionary of url of mobile and desktop lotties per slug id
-// structure: "slug" = ["mobile", "desktop"]
-  
 
 const onReady = () => {
     pageLoaded = true
-    logCareers()
-    readyPreloader()
-    initProjectLotties()
-    copyEmail()
-    initCms()
+
+    logCareers() // logs a frog and message to the console
+    readyPreloader() // hides preloader and add event listener for frog lottie
+    initProjectLotties() // initiates project lotties for home and work pages
+    copyEmail() // copies email to clipboard in footer
+    initCms() // sets color hovers and cms filtering style for work page & content hub
+    document.querySelector('.landing-video-container') && loadAnim() // for home page intro anim
 }
 
 const onLoading = () => {
